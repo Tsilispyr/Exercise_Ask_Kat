b388b61a88a5d5c7833bb5140050aa5e1e31992d
BlackThest <dimitrisapostolakis319@gmail.com>
2025-02-13 18:53:12 +0200

    Add files via upload

---
+++ Ask/src/main/java/com/example/Ask/config/SecurityConfig.java
@@ -0,0 +1,88 @@
+package com.example.Ask.config;
+
+import com.example.Ask.Entities.Role;
+import com.example.Ask.Entities.User;
+import com.example.Ask.Repositories.RoleRepository;
+import com.example.Ask.Repositories.UserRepository;
+import com.example.Ask.Service.UserService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    private RoleRepository roleRepository;
+    private UserService userService;
+
+    private UserDetailsService userDetailsService;
+
+    private BCryptPasswordEncoder passwordEncoder;
+    public SecurityConfig(UserService userService, UserDetailsService userDetailsService, BCryptPasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+        this.userService = userService;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home", "/register", "/saveUser", "/images/**", "/js/**", "/css/**").permitAll()
+                        .requestMatchers("/Requests").hasRole("ADMIN")
+                        .requestMatchers("/Requests").hasRole("DOCTOR")
+                        .requestMatchers("/Requests").hasRole("SHELTER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/Animal", true)
+                        .permitAll())
+                .logout((logout) -> logout.permitAll());
+        return http.build();
+    }
+
+
+}
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final CustomAuthenticationFailureHandler failureHandler;
+//    private BCryptPasswordEncoder passwordEncoder;
+//
+//    public SecurityConfig(CustomAuthenticationFailureHandler failureHandler) {
+//        this.failureHandler = failureHandler;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(requests -> requests
+//                        .requestMatchers("/", "/home", "/register", "/saveUser", "/images/**", "/js/**", "/css/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(form -> form
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/Animal", true)
+//                        .failureHandler(failureHandler) // Attach custom failure handler
+//                        .permitAll()
+//                )
+//                .logout(logout -> logout.permitAll());
+//
+//        return http.build();
+//    }
+//
+//
+//}
